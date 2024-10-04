495d2887b85b73b7b8f68dace4ac006b7b331383
Erick <moralerick90@gmail.com>
2024-10-04 14:49:20 -0700

    added class structures

---
+++ ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+
+public class Scripture
+{
+    // Constructor
+    Reference ref = new Reference();
+    Word w = new Word();
+
+    // Attributes
+    private Reference
+    private List<Word>
+
+    // Methods
+    private void HideRandomWords(numberToHide:int)
+    {}
+    public string GetDisplayText()
+    {}
+
+}
---
+++ ScriptureMemorizer/Word.cs
@@ -0,0 +1,17 @@
+using System:
+using System.IO;
+
+public class World
+{
+    // Attributes
+
+    // Methods
+    private void Hide()
+    {}
+    private void Show()
+    {}
+    private bool IsHidden()
+    {}
+    public string GetDisplayText()
+    {}
+}
